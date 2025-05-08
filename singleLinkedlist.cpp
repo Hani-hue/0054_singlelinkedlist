658d3f8a0b0cf9bac31847e4baa64382f1f59ec1
Hani-hue <isnahabibah8729@gmail.com>
2025-05-08 19:50:31 +0700

    bikin logika node

--- singleLinkedlist.cpp
+++ singleLinkedlist.cpp
@@ -18,4 +18,22 @@ public:
     {
         START = NULL;
     }
+    void addNote()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        }
+    }
 }
